7029aa9a589c9f18d337e28a630d09525058b2af
Raúl Otaño Hurtado <rulyotano@gmail.com>
2021-07-09 21:44:15 +0200

    Added ordering behavior

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/UnhandlerExceptionBehavior.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviors
+{
+    public class UnhandlerExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandlerExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, 
+            CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError("Application Request: Unhandled exception for the request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
