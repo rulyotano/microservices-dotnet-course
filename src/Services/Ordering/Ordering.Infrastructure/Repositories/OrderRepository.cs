10ff2183b820ce0f18733a71fdff725560e665d0
Raúl Otaño Hurtado <rulyotano@gmail.com>
2021-07-20 21:01:40 +0200

    Done infrastructure implementations for repositories and email service

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName) =>
+            await _dbContext.Set<Order>().Where(order => order.UserName == userName).ToListAsync();
+        
+    }
+}
