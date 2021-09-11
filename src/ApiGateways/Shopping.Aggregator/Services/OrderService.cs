5f38a5d328cc49a65f5e3ff38e1175fda690accf
Raul <rulyotano@gmail.com>
2021-09-11 13:45:46 +0200

    Working in progress in shopping aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
