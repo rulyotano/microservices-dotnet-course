3217db6997bec61d5506dc7eaa4915afb4440750
Raúl Otaño Hurtado <rulyotano@gmail.com>
2021-09-12 16:52:34 +0200

    Working on web app

---
+++ src/WebApps/AspnetRunBasics/Models/BasketModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemModel> Items { get; set; } = new List<BasketItemModel>();
+        public decimal TotalPrice { get; set; }
+    }
+    public class BasketItemModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
